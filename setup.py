1f3218c6cbf1ca9e5252bc197ace31e64cb4a889
ElTinmar <martin.privat@hotmail.fr>
2025-09-01 18:23:21 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='thorlabs_ccs',
     author='Martin Privat',
-    version='0.2.18',
+    version='0.2.19',
     packages=['thorlabs_ccs', 'thorlabs_pmd'],
     license='LGPL2.1',
     description='Control Thorlabs devices',
