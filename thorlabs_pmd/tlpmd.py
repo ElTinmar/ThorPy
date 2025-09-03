1412c70bc3357c3e15632850474b3048f1d05a05
ElTinmar <martin.privat@hotmail.fr>
2025-09-03 14:40:30 +0200

    polish a little bit

--- thorlabs_pmd/tlpmd.py
+++ thorlabs_pmd/tlpmd.py
@@ -1,7 +1,7 @@
 import usb.core
 import usbtmc
 import sys
-from typing import List, NamedTuple, Tuple
+from typing import List, NamedTuple
 
 if sys.platform == 'win32':
     import libusb_package
@@ -11,22 +11,8 @@ else:
 
 REN_CONTROL = 160 # Optional. Mechanism to enable or disable local controls on a device.
 GO_TO_LOCAL = 161 # Optional. Mechanism to enable local controls on a device. 
-
-class DeviceNotFound(Exception): ...
-
 THORLABS_VID = 0x1313
-PM100D_PID = 0x8078
-LINE_FREQUENCY_EUROPE = 50
-S120C_APERTURE_SIZE_MM = 9.5
 PID_RANGE = (0x8078, 0x8079) # TODO check actual values
-area = 3.14159 * (S120C_APERTURE_SIZE_MM/2)**2
-
-# S120C 
-# Active Detector Area 	9.7 mm x 9.7 mm
-# Aperture Size 	Ø9.5 mm
-# to display the power or energy density or the light intensity value in W/cm² or
-# J/cm² in the right sub display it is necessary to enter the diameter of the incident
-# beam or at an overfilled sensor the diameter of the sensor aperture.
 
 class DevInfo(NamedTuple):
     vid: int
@@ -62,7 +48,7 @@ class TLPMD:
         self.instr = usbtmc.Instrument(device_info.vid, device_info.pid, device_info.serial_number)
         self.instr.clear = lambda: None
         self.instr.open()
-        self.instr.device.ctrl_transfer(bmRequestType=0xA1, bRequest=REN_CONTROL, wValue=0x0001, wIndex=0x0000, data_or_wLength=1)
+        self.remote_enable(1)
         self.initialize()
         
     def initialize(self): 
@@ -73,84 +59,11 @@ class TLPMD:
         self.instr.write("SENS:AVER:COUN 1")
         self.check_error_code()
 
-        # self.instr.write("*CLS;*SRE 0;*ESE 0;:STAT:PRES")
-        # self.check_error_code()
+    def remote_enable(self, value: int) -> None:
+        self.instr.device.ctrl_transfer(bmRequestType=0xA1, bRequest=REN_CONTROL, wValue=value, wIndex=0x0000, data_or_wLength=1)
 
-        # print(self.instr.ask("SYST:SENS:IDN?"))
-        # print(self.instr.ask("STAT:AUX:COND?"))
-        # print(self.instr.ask("*IDN?"))
-        # print(self.instr.ask("CAL:STR?"))
-
-        # print(self.instr.ask("SYST:SENS:IDN?"))    
-        # print(self.instr.ask("SENS:CORR:WAV? MIN"))
-        # print(self.instr.ask("SENS:CORR:WAV? MAX"))
-        
-        # self.instr.write("ABOR")
-        # self.instr.write("CONF:Current")
-
-        # print(self.instr.ask("SENS:CORR?"))
-        # print(self.instr.ask("SENS:CORR? MIN"))
-        # print(self.instr.ask("SENS:CORR? MAX"))
-        # print(self.instr.ask("SENS:CORR:BEAM?"))
-        # print(self.instr.ask("SENS:CORR:BEAM? MIN"))
-        # print(self.instr.ask("SENS:CORR:BEAM? MAX"))
-        # print(self.instr.ask("SENS:CORR:WAV?"))
-        # print(self.instr.ask("SENS:CORR:WAV? MIN"))
-        # print(self.instr.ask("SENS:CORR:WAV? MAX"))
-        # print(self.instr.ask("SENS:CORR:POW:PDI:RESP?"))
-        # print(self.instr.ask("INP:FILT?"))
-        # print(self.instr.ask("SENS:CORR:COLL:ZERO:MAGN?"))
-        # print(self.instr.ask("SENS:CURR:RANG:AUTO?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP? MIN"))
-
-        # self.instr.write("SENS:AVER:COUN 1")
-        # self.check_error_code()
-        # print(self.instr.ask("SENS:CURR:RANG:UPP?"))
-        # print(self.instr.ask("Read?"))
-
-        # print(self.instr.ask("SENS:CORR?"))
-        # print(self.instr.ask("SENS:CORR? MIN"))
-        # print(self.instr.ask("SENS:CORR? MAX"))
-        # print(self.instr.ask("SENS:CORR:BEAM?"))
-        # print(self.instr.ask("SENS:CORR:BEAM? MIN"))
-        # print(self.instr.ask("SENS:CORR:BEAM? MAX"))
-        # print(self.instr.ask("SENS:CORR:WAV?"))
-        # print(self.instr.ask("SENS:CORR:WAV? MIN"))
-        # print(self.instr.ask("SENS:CORR:WAV? MAX"))
-        # print(self.instr.ask("SENS:CORR:POW:PDI:RESP?"))
-        # print(self.instr.ask("INP:FILT?"))
-        # print(self.instr.ask("SENS:CORR:COLL:ZERO:MAGN?"))
-        # print(self.instr.ask("SENS:CURR:RANG:AUTO?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP? MIN"))
-
-        # print(self.instr.ask("SENS:CORR:BEAM?"))
-        # print(self.instr.ask("SENS:CORR:BEAM?"))
-
-        # print(self.instr.ask("SENS:CORR?"))
-        # print(self.instr.ask("SENS:CORR? MIN"))
-        # print(self.instr.ask("SENS:CORR? MAX"))
-        # print(self.instr.ask("SENS:CORR:BEAM?"))
-        # print(self.instr.ask("SENS:CORR:BEAM? MIN"))
-        # print(self.instr.ask("SENS:CORR:BEAM? MAX"))
-        # print(self.instr.ask("SENS:CORR:WAV?"))
-        # print(self.instr.ask("SENS:CORR:WAV? MIN"))
-        # print(self.instr.ask("SENS:CORR:WAV? MAX"))
-        # print(self.instr.ask("SENS:CORR:POW:PDI:RESP?"))
-        # print(self.instr.ask("INP:FILT?"))
-        # print(self.instr.ask("SENS:CORR:COLL:ZERO:MAGN?"))
-        # print(self.instr.ask("SENS:CURR:RANG:AUTO?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP? MIN"))
-        
-        # print(self.instr.ask("SENS:CURR:RANG:AUTO?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP?"))
-        # print(self.instr.ask("SENS:CURR:RANG:UPP? MIN"))
-        # print(self.instr.ask("Read?"))
-        # print(self.instr.ask("Read?"))
-        # print(self.instr.ask("Read?"))
-        # print(self.instr.ask("Read?"))
+    def local_control(self) -> None:
+        self.instr.device.ctrl_transfer(bmRequestType=0xA1, bRequest=GO_TO_LOCAL, wValue=0x0000, wIndex=0x0000, data_or_wLength=1)
 
     def check_error_code(self) -> None:
         error = self.instr.ask("SYST:ERR?")
@@ -196,8 +109,8 @@ class TLPMD:
         return self.get_power_mW()/area_cm2
 
     def close(self) -> None:
-        self.instr.device.ctrl_transfer(bmRequestType=0xA1, bRequest=GO_TO_LOCAL, wValue=0x0000, wIndex=0x0000, data_or_wLength=1)
-        self.instr.device.ctrl_transfer(bmRequestType=0xA1, bRequest=REN_CONTROL, wValue=0x0000, wIndex=0x0000, data_or_wLength=1)
+        self.local_control()
+        self.remote_enable(0)
         self.instr.close()
         self.instr = None
 
